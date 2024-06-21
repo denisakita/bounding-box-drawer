fa9c086bedf4f30bd4f2da044a3703921aacad82
Denisa Kita <denisakita7@gmail.com>
2024-06-21 08:39:17 +0200

    Add components and routes at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import LandingPage from "./components/LandingPage";
+import BoundingBoxEditor from "./components/BoundingBoxEditor";
 
 function App() {
     return (
-        <h1>hello</h1>
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage/>}/>
+                <Route path="/editor" element={<BoundingBoxEditor/>}/>
+            </Routes>
+        </Router>
     );
 }
 
